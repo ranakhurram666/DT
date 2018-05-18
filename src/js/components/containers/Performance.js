0f0e48c6c76ea384b28cc0a21eb46f6a291c53fd
Muhammad Ishfaq <ishfaq.sardar@systemsltd.com>
2018-05-18 15:35:40 +0500

    Tabular structure added

--- src/js/components/containers/Performance.js
+++ src/js/components/containers/Performance.js
@@ -58,51 +58,99 @@ render() {
 			<div className="tablular-data-wrapper clearfix">
 				<div className="col-md-6">
 					<table class="table table-striped">
-	<thead>
-		<tr>
-			<th>Categories</th>
-			<th>Total Listeners</th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<td><span class="order">1</span> News</td>
-				<td>
-					34,597 
-					<span className="range sucess"><i className="fas fa-arrow-up"></i>10.6 %</span>
-				</td>
-			</tr>
-			<tr>
-				<td><span class="order">2</span> Drama</td>
-					<td>
-						4,495
-						<span className="range sucess"><i className="fas fa-arrow-up"></i>8.4 %</span>
-					</td>
-				</tr>
-				<tr>
-					<td><span class="order">3</span> Kids Stories</td>
-						<td>
-							2,115
-							<span className="range sucess"><i className="fas fa-arrow-up"></i>8.4 %</span>
-						</td>
-					</tr>
-					<tr>
-						<td><span class="order">4</span> Cricket</td>
-							<td>
-								1,597
-								<span className="range sucess"><i className="fas fa-arrow-up"></i>5.9 %</span>
-							</td>
-						</tr>
-						<tr>
-							<td><span class="order">5</span> Tips &amp; Tricks</td>
-								<td>1,030
-									<span className="range sucess"><i className="fas fa-arrow-up"></i>9.7 %</span>
+						<thead>
+							<tr>
+								<th>Categories</th>
+								<th>Total Listeners</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td><span class="order">1</span> News</td>
+								<td>
+									34,597 
+									<span className="range sucess"><i className="fas fa-arrow-up"></i>10.6 %</span>
 								</td>
 							</tr>
-						</tbody>
-					</table>
-					<div className="more-categories"><a href="">More Categories Details <i className="fas fa-chevron-right"></i></a></div>
-				</div>
+							<tr>
+								<td><span class="order">2</span> Drama</td>
+								<td>
+									4,495
+									<span className="range sucess"><i className="fas fa-arrow-up"></i>8.4 %</span>
+								</td>
+							</tr>
+								<tr>
+									<td><span class="order">3</span> Kids Stories</td>
+									<td>
+										2,115
+										<span className="range sucess"><i className="fas fa-arrow-up"></i>8.4 %</span>
+									</td>
+								</tr>
+								<tr>
+									<td><span class="order">4</span> Cricket</td>
+									<td>
+										1,597
+										<span className="range sucess"><i className="fas fa-arrow-up"></i>5.9 %</span>
+									</td>
+								</tr>
+								<tr>
+									<td><span class="order">5</span> Tips &amp; Tricks</td>
+									<td>1,030
+										<span className="range sucess"><i className="fas fa-arrow-up"></i>9.7 %</span>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<div className="more-categories"><a href="">More Categories Details <i className="fas fa-chevron-right"></i></a></div>
+					</div>
+
+					<div className="col-md-6">
+					<table class="table table-striped">
+						<thead>
+							<tr>
+								<th>Categories</th>
+								<th>Total Listeners</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td><span class="order">1</span> News</td>
+								<td>
+									34,597 
+									<span className="range sucess"><i className="fas fa-arrow-up"></i>10.6 %</span>
+								</td>
+							</tr>
+							<tr>
+								<td><span class="order">2</span> Drama</td>
+								<td>
+									4,495
+									<span className="range sucess"><i className="fas fa-arrow-up"></i>8.4 %</span>
+								</td>
+							</tr>
+								<tr>
+									<td><span class="order">3</span> Kids Stories</td>
+									<td>
+										2,115
+										<span className="range sucess"><i className="fas fa-arrow-up"></i>8.4 %</span>
+									</td>
+								</tr>
+								<tr>
+									<td><span class="order">4</span> Cricket</td>
+									<td>
+										1,597
+										<span className="range sucess"><i className="fas fa-arrow-up"></i>5.9 %</span>
+									</td>
+								</tr>
+								<tr>
+									<td><span class="order">5</span> Tips &amp; Tricks</td>
+									<td>1,030
+										<span className="range sucess"><i className="fas fa-arrow-up"></i>9.7 %</span>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<div className="more-categories"><a href="">More Categories Details <i className="fas fa-chevron-right"></i></a></div>
+					</div>
 			</div>
     </section>
     );
