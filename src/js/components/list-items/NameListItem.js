669d3745a4a62114573f037e437bd2306a72fa67
Khurram Irshad <khurram.irshad@systemsltd.com>
2018-05-23 11:58:06 +0500

    DT: Added Categories List

---
+++ src/js/components/list-items/NameListItem.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { connect } from "react-redux"
+/*
+@connect((store) => {
+  return {
+    user: store.user.user,
+    userFetched: store.user.fetched,
+    tweets: store.tweets.tweets,
+  };
+})*/
+export default class NameListItem extends React.Component {
+  constructor() {
+    super();
+  }
+  componentWillMount() {
+    //this.props.dispatch(fetchUser())
+  }
+  getRiseAndFall(rise) {
+    if (rise) {
+      return <span className="range sucess"><i className="fas fa-arrow-up"></i></span>;
+    } else {
+      return <span className="range danger"><i className="fas fa-arrow-down"></i></span>;
+    }
+  }
+
+  render() {
+        return (
+                <td><span className="range sucess"> {this.getRiseAndFall(this.props.rise)} </span> {this.props.name} </td>
+        );
+  }
+}
